561b789ab238ccceea4b999bd88c66fa7dcf80cc
AdrianNwajuobi1991 <adrian.nwajuobi@gmail.com>
2015-09-04 02:06:30 -0400

    Added Virginia and Washington,DC total cost calculators.

--- EastCoastUnitedStatesTotalPriceCalculator/src/com/adrian/abstractandmodelclasses/package-info.java
+++ EastCoastUnitedStatesTotalPriceCalculator/src/com/adrian/abstractandmodelclasses/package-info.java
@@ -2,7 +2,7 @@
  * This class will contain all Java Abstract or Model classes for this application
  */
 /**
- * @author Adrian
+ * @author Adrian Nwajuobi
  *
  */
 package com.adrian.abstractandmodelclasses;
