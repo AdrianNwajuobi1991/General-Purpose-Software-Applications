bbb133d7083c2613204162a31f731ecc64d06ee9
AdrianNwajuobi1991 <adrian.nwajuobi@gmail.com>
2015-09-04 00:47:55 -0400

    Initial commit of a Java Application that Calculates the total cost of a given item in any of the states of the U.S East Coast.

---
+++ EastCoastUnitedStatesTotalPriceCalculator/src/com/adrian/abstractclasses/CommodityItem.java
@@ -0,0 +1,46 @@
+/**
+ * A general Abstract class representing A general consumer item for purchase
+ */
+package com.adrian.abstractclasses;
+
+import com.adrian.interfaces.EastCoastTotalCostCalculator;
+
+/**
+ * @author Adrian Nwajuobi
+ *
+ */
+public abstract class CommodityItem {
+
+	/**
+	 * eastCoastTaxCalculatorHolds -- is a field that references an instance of
+	 */
+	protected EastCoastTotalCostCalculator eastCoastTotalCostCalculator;
+	/**
+	 * basePrice -- is a field to denote the MSRP of the product
+	 */
+	protected float basePrice;
+	/**
+	 * name -- is a field that represents the name of the item
+	 */
+	protected String name;
+	
+	/**
+	 * doTotalPriceCalculation -- delegates the calculation of the Total Price of an item to the 
+	 * eastCoastTaxCalculator
+	 */
+	public void doTotalPriceCalculation(){
+		eastCoastTotalCostCalculator.calculatePricePlusTax(basePrice);
+	}
+	/**
+	 * 
+	 * @param eastCoastTotalCostCalculator - the reference to the desired total cost calculator to use
+	 * setTotalCostCalculator -- a setter method(mutator) that dynamically sets the total calculator at runtime
+	 */
+	public void setTotalCostCalculator(EastCoastTotalCostCalculator eastCoastTotalCostCalculator){
+		this.eastCoastTotalCostCalculator = eastCoastTotalCostCalculator;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
---
+++ EastCoastUnitedStatesTotalPriceCalculator/src/com/adrian/abstractclasses/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This class will contain all Java Abstract classes for this application
+ */
+/**
+ * @author Adrian
+ *
+ */
+package com.adrian.abstractclasses;
