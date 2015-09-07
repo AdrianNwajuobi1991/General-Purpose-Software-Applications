552b2d5a8aa4584f4271cb34929565fc6c146252
AdrianNwajuobi1991 <adrian.nwajuobi@gmail.com>
2015-09-07 17:19:45 -0400

    initial commint of this application

---
+++ MarylandVirginiaDistrictOfColumbiaPriceCalculatorGUIClient/src/com/adrian/ApplicationGUI/PriceCalculatorClientGUI.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.adrian.ApplicationGUI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ * @author Adrian Nwajuobi
+ *
+ */
+public class PriceCalculatorClientGUI {
+
+	/**
+	 * The main JFrame of the GUI
+	 */
+	private JFrame applicationFrame;
+	/**
+	 * the button that sends data to a Price Calculation Server for evaluation
+	 */
+	private JButton sendForPriceCalculation;
+	
+}
