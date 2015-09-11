f29dcab612ab707b3a3c3f5ef9d5007cd7fcd3fb
AdrianNwajuobi1991 <adrian.nwajuobi@gmail.com>
2015-09-11 00:54:27 -0400

    Delete package-info.java

--- SimpleEchoClient/src/com/echoclient/constants/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This Package holds constants for IP address and Port numbers for Servers deployed on the remote platform
- */
-/**
- * @author Adrian Nwajuobi
- *
- */
-package com.echoclient.constants;
