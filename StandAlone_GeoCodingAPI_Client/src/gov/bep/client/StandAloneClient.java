0f6a14231d03da689a1d2a442cd50eb3ea919ab7
anwajuobi <anwajuobi@deloitte.com>
2017-11-21 14:00:24 -0500

    testing build triggers.

--- StandAlone_GeoCodingAPI_Client/src/gov/bep/client/StandAloneClient.java
+++ StandAlone_GeoCodingAPI_Client/src/gov/bep/client/StandAloneClient.java
@@ -35,7 +35,7 @@ public class StandAloneClient {
 			ex.printStackTrace();
 		}
 		String result = callGeoCodeAPI(prop.getProperty("json_or_xml"), prop.getProperty("api_key"), prop.getProperty("address"));
-		System.out.println(result+"Please work now");
+		System.out.println(result);
 		
 	}
 	
